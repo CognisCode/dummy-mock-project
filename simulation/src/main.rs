a5873c7f0d51abdb32b8d4b8798fdef1381a9a83
Yassin Yassin <yassin001@live.nl>
2024-09-22 21:29:23 +0200

    base simulation

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -1,3 +1,129 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+use nannou::prelude::*;
+
+mod player;
+use player::{Player, PlayerType};
+
 fn main() {
-    println!("Hello, world!");
+    let mut chaser_score: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
+    // this will be a separate thread
+    nannou::app(simulation).update(next_step).run();
+}
+
+const SMARTCHASER: usize = 1;
+const CLOSECHASER: usize = 1; 
+const HIGHCHASER: usize = 1; 
+const HIGHREWARDS: usize = 50; 
+const LOWREWARDS: usize = 50; 
+ 
+pub const HIGHVALUE: f32 = 200.0;
+pub const LOWVALUE: f32 = 50.0;
+
+struct Simulation {
+    all_players_vector: Vec<Player>, 
+}
+
+fn simulation(app: &App) -> Simulation {
+
+        // Initialize the window
+        app.new_window()
+        .size(2400, 1200)
+        .view(view)
+        .build()
+        .unwrap();
+
+    let mut all_players_vector = Vec::new();
+
+    // Create HighReward and LowReward
+    create_players(1, HIGHREWARDS, PlayerType::HighReward, HIGHVALUE, &mut all_players_vector);
+    create_players(100, LOWREWARDS, PlayerType::LowReward, LOWVALUE, &mut all_players_vector);
+
+    // Create chasers
+    create_chasers(HIGHCHASER, PlayerType::ChaseHighest, &mut all_players_vector);
+    create_chasers(CLOSECHASER, PlayerType::ChaseClosest, &mut all_players_vector);
+    create_chasers(SMARTCHASER, PlayerType::ChaseSmart, &mut all_players_vector);
+
+    Simulation { all_players_vector }
+}
+
+fn next_step(app: &App, simulation: &mut Simulation, _update: Update) {
+
+    for i in 0..simulation.all_players_vector.len() {
+        
+        let highrewards = simulation.all_players_vector[i].rewards(&simulation.all_players_vector, i, PlayerType::HighReward); 
+        let lowrewards = simulation.all_players_vector[i].rewards(&simulation.all_players_vector, i, PlayerType::LowReward); 
+        let players = simulation.all_players_vector[i].players(&simulation.all_players_vector, i); 
+
+        match simulation.all_players_vector[i].player_type {
+            PlayerType::ChaseHighest => {
+                let chase_result = simulation.all_players_vector[i].chase_highest_rewards(&highrewards, &lowrewards);
+                simulation.all_players_vector[i].direction += chase_result.direction;
+                simulation.all_players_vector[i].target_id = chase_result.target_id;
+            }
+            PlayerType::ChaseClosest => {
+                let chase_result = simulation.all_players_vector[i].chase_closest_rewards(&highrewards, &lowrewards);
+                simulation.all_players_vector[i].direction += chase_result.direction;
+                simulation.all_players_vector[i].target_id = chase_result.target_id;
+            }
+            PlayerType::ChaseSmart => {
+                let chase_result = simulation.all_players_vector[i].chase_smart_rewards(&highrewards, &lowrewards);
+                simulation.all_players_vector[i].direction += chase_result.direction;
+                simulation.all_players_vector[i].target_id = chase_result.target_id;
+            }
+            PlayerType::HighReward | PlayerType::LowReward => {
+                
+                if simulation.all_players_vector[i].get_consumed(&players) {
+                    simulation.all_players_vector[i].player_type = PlayerType::Consumed;
+                    // update score
+                }
+                simulation.all_players_vector[i].direction += vec2(0.0, 0.0);
+            }
+            _ => {}
+        }
+
+        simulation.all_players_vector[i].update(); 
+        // prevent players to leave the screen
+        let screen_right = app.window_rect().right() as f32; // half of width pixels
+        let screen_top = app.window_rect().top() as f32; // half of height pixels
+        simulation.all_players_vector[i].edge(screen_top, screen_right);
+    }
+}
+
+fn view(app: &App, model: &Simulation, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(BEIGE);
+    for i in 0..model.all_players_vector.len() {
+        model.all_players_vector[i].show(&draw); 
+    }
+    draw.to_frame(app, &frame).unwrap();
+}
+
+fn create_player(id: i16, player_type: PlayerType, value: f32) -> Player {
+    let player = Player::new(
+        (random_f32() - 0.5) * 1800.0, 
+        (random_f32() - 0.5) * 800.0,
+        20.0,
+        player_type,
+        id,
+        value
+    );    
+    player
+}
+
+fn create_players(id_start: i16, count: usize, player_type: PlayerType, value: f32, all_players: &mut Vec<Player>) {
+    let mut id = id_start;
+    for _ in 0..count {
+        let player = create_player(id, player_type.clone(), value);
+        id += 1;
+        all_players.push(player);
+    }
+}
+
+fn create_chasers(count: usize, player_type: PlayerType, all_players: &mut Vec<Player>) {
+    for _ in 0..count {
+        let player = create_player(0, player_type.clone(), 0.0);
+        all_players.push(player);
+    }
 }
---
+++ simulation/src/player.rs
@@ -0,0 +1,269 @@
+use std::ops::{Div, Mul, Sub};
+
+use nannou::prelude::*;
+
+use crate::{HIGHVALUE, LOWVALUE};
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum PlayerType {
+    ChaseClosest,
+    ChaseHighest,
+    ChaseSmart,
+    Consumed,
+    HighReward,
+    LowReward
+}
+
+#[derive(Debug)]
+pub struct Player {
+    pub position: Vec2, // 2D-vector
+    pub angle_vec: Vec2,
+    pub player_type: PlayerType,
+    pub direction: Vec2,
+    size: f32, 
+    max_step_size: f32,
+    pub id: i16,
+    pub value: f32,
+    pub target_id: i16
+}
+
+pub struct TargetGoal {
+    pub direction: Vec2,
+    pub target_id: i16,
+}
+
+impl Player {
+    pub fn new(pos_x: f32, pos_y: f32, size: f32, player_type: PlayerType, id: i16, value: f32) -> Player {
+        
+        let max_step_size = match player_type {
+            PlayerType::ChaseHighest => {3.0},
+            PlayerType::ChaseClosest => {3.0},
+            PlayerType::ChaseSmart => {3.0},
+            PlayerType::Consumed => {0.0},
+            PlayerType::HighReward => {0.0},
+            PlayerType::LowReward => {0.0},
+        };
+     
+        Player {
+                position: vec2(pos_x, pos_y),
+                angle_vec: vec2(0.0, 0.0),
+                size,
+                direction: vec2(0.0, 0.0),
+                max_step_size,
+                player_type,
+                id,
+                value,
+                target_id: 0
+        }
+    }
+
+    pub fn show(&self, draw: &Draw) {
+
+        let red;
+        let green;
+        let blue;
+        
+        match self.player_type {
+            PlayerType::ChaseHighest => {
+                red = 255.0;
+                green = 0.0;
+                blue = 0.0;
+            },
+            PlayerType::ChaseClosest => {
+                red = 139.0;
+                green = 0.0;
+                blue = 139.0;
+            },
+            PlayerType::HighReward => {
+                red = 0.0;
+                green = 255.0;
+                blue = 0.0;
+            },
+            PlayerType::LowReward => {
+                red = 0.0;
+                green = 0.0;
+                blue = 255.0;
+            },
+            PlayerType::ChaseSmart => {
+                red = 0.0;
+                green = 0.0;
+                blue = 0.0;
+            },
+            PlayerType::Consumed => {
+                red = 255.0;
+                green = 255.0;
+                blue = 0.0;
+            }
+        };
+        
+        if self.player_type != PlayerType::HighReward && self.player_type != PlayerType::LowReward && self.player_type != PlayerType::Consumed {
+            draw.tri()
+                .w_h(self.size, self.size)
+                .x_y(self.position.x, self.position.y)
+                .rotate(self.angle_vec.angle()) // angle of the triangle
+                .rgba(red, green, blue, 0.85);
+        } else {
+            draw.ellipse()
+                .w_h(self.size, self.size)
+                .x_y(self.position.x, self.position.y)
+                .rgba(red, green, blue, 0.85);
+        }
+
+
+    }
+
+    pub fn update(&mut self) {
+
+        if self.player_type != PlayerType::HighReward && self.player_type != PlayerType::LowReward && self.player_type != PlayerType::Consumed {
+            
+            self.position += self.direction.normalize() * 3.0;   
+            // angle vector is previous direction + new direction and then normalized to keep te step size equal 
+            self.angle_vec += self.direction;
+            self.angle_vec = self.angle_vec.clamp_length(self.max_step_size,self.max_step_size);
+        }
+        self.direction = vec2(0.0, 0.0); // reset accelartion
+    }
+
+    pub fn edge(&mut self, top: f32, right: f32) {
+        if self.position.x > right {
+            self.position.x = -right;
+        } else if self.position.x < -right {
+            self.position.x = right
+        }
+        if self.position.y > top {
+            self.position.y = -top;
+        } else if self.position.y < -top {
+            self.position.y = top
+        }
+    }
+
+
+    pub fn rewards<'a>(&self, all_players: &'a Vec<Player>, player_index: usize, player_type: PlayerType) -> Vec<&'a Player> {
+        
+        let mut rewards = Vec::new();
+
+        for i in 0..all_players.len() {
+
+            if i != player_index  && all_players[i].player_type == player_type {
+                rewards.push(&all_players[i]);
+            }
+        }
+        rewards
+    }
+
+    pub fn players<'a>(&self, all_players: &'a Vec<Player>, player_index: usize) -> Vec<&'a Player> {
+        
+        let mut players = Vec::new();
+        for i in 0..all_players.len() {
+
+            if i != player_index  && all_players[i].player_type != PlayerType::HighReward && all_players[i].player_type != PlayerType::Consumed && all_players[i].player_type != PlayerType::LowReward {
+                players.push(&all_players[i]);
+            }
+        }
+        players
+    }
+
+    pub fn get_consumed(&self, chasers: &Vec<&Player>) -> bool {
+        let len = chasers.len();
+
+        if len == 0 {
+            return false;
+        }
+        for i in chasers {
+            if self.position.distance(i.position) < 30.0 && self.id == i.target_id {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn chase_closest_rewards(&self, highrewards: &Vec<&Player>, lowrewards: &Vec<&Player>) -> TargetGoal {
+
+        if highrewards.len() + lowrewards.len() == 0 { 
+           return TargetGoal{direction: vec2(0.0, 0.0), target_id: 0} 
+        }
+                
+        // find nearest reward
+        let mut shortest_distance = 100000000.0;
+        let mut direction = Vec2::new(0.0, 0.0);
+
+        let mut target_id = 0;
+        // find closest traget
+        for reward in highrewards {
+            if reward.position.distance(self.position) < shortest_distance {    
+                direction = reward.position.sub(self.position).normalize();
+                shortest_distance = reward.position.distance(self.position);
+                target_id = reward.id; 
+            }   
+        }
+
+        for reward in lowrewards {
+            if reward.position.distance(self.position) < shortest_distance {    
+                direction = reward.position.sub(self.position).normalize();
+                shortest_distance = reward.position.distance(self.position);
+                target_id = reward.id; 
+            }   
+        }
+
+        return TargetGoal{direction: Vec2::new(direction.x , direction.y ), target_id}         
+    }
+
+    pub fn chase_highest_rewards(&self, highrewards: &Vec<&Player>, lowrewards: &Vec<&Player>) -> TargetGoal {
+        // return closest reward coordinates
+        let rewards: &Vec<&Player>;
+
+        if highrewards.len() != 0 { 
+            rewards = highrewards;
+        } else if lowrewards.len() != 0{
+            rewards = lowrewards;
+        } else {
+            return TargetGoal{direction: vec2(0.0, 0.0), target_id: 0} 
+        }
+        
+        let mut shortest_distance = 100000000.0;
+        let mut direction = Vec2::new(0.0, 0.0);
+        let mut target_id = 0;
+
+        for reward in rewards {
+
+            if reward.position.distance(self.position) < shortest_distance {    
+                direction = reward.position.sub(self.position).normalize();
+                shortest_distance = reward.position.distance(self.position);
+                target_id = reward.id; 
+            }   
+        }
+        return TargetGoal{direction: Vec2::new(direction.x , direction.y ), target_id}         
+    }
+
+
+pub fn chase_smart_rewards(&self, highrewards: &Vec<&Player>, lowrewards: &Vec<&Player>) -> TargetGoal {
+
+    if highrewards.len() + lowrewards.len() == 0 { 
+        return TargetGoal{direction: vec2(0.0, 0.0), target_id: 0} 
+     }
+             
+     let mut most_valuable = 0.0;
+     let mut direction = Vec2::new(0.0, 0.0);
+     let mut target_id = 0;
+     
+     for reward in highrewards {
+
+         if HIGHVALUE / reward.position.distance(self.position) > most_valuable {    
+             direction = reward.position.sub(self.position).normalize();
+             most_valuable = HIGHVALUE / reward.position.distance(self.position);
+             target_id = reward.id; 
+         }
+
+     }
+
+     for reward in lowrewards {
+         if LOWVALUE / reward.position.distance(self.position) > most_valuable {    
+             direction = reward.position.sub(self.position).normalize();
+             most_valuable =  LOWVALUE / reward.position.distance(self.position);
+             target_id = reward.id; 
+         }   
+     }
+
+     return TargetGoal{direction: Vec2::new(direction.x , direction.y ), target_id}               
+}
+}
