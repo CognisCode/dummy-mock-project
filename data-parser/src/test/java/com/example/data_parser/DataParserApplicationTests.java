fdc4958bde75cdf0445e107ad545ac8082db38a7
Yassin Yassin <yassin001@live.nl>
2024-09-26 16:40:59 +0200

    added docker support for spring app (data-parser)

--- data-parser/src/test/java/com/example/data_parser/DataParserApplicationTests.java
+++ data-parser/src/test/java/com/example/data_parser/DataParserApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.data_parser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DataParserApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class DataParserApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
